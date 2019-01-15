2d79b4128ad70b1b2030b49193ad4b33630aff4d
Trần Trung Hiếu <hieu.trantrung1204@icloud.com>
2019-01-15 11:38:05 +0700

    + create window use windows API

--- source/win32/main.cpp
+++ source/win32/main.cpp
@@ -1,4 +1,9 @@
+
 #include <windows.h>
+
+// the WindowProc function prototype
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+void ProcessKey(const char* type, WPARAM wParam);
 
 // the entry point for any Windows program
 int WINAPI WinMain(HINSTANCE hInstance,
@@ -6,12 +11,108 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	LPSTR lpCmdLine,
 	int nCmdShow)
 {
-	// create a "Hello World" message box using MessageBox()
-	MessageBox(NULL,
-		"Hello-Game",
-		"Just another Hello World program!",
-		MB_ICONEXCLAMATION | MB_OK);
+	HWND hWnd;
+	WNDCLASSEX wc;
 
-	// return 0 to Windows
+	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.lpszClassName = "WindowClass";
+
+	RegisterClassEx(&wc);
+
+	RECT wr = { 0, 0, 800, 600 };
+	AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);
+
+	hWnd = CreateWindowEx(NULL,
+		"WindowClass",
+		"Hello-Game",
+		WS_OVERLAPPEDWINDOW,
+		300,
+		300,
+		wr.right - wr.left,
+		wr.bottom - wr.top,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	ShowWindow(hWnd, nCmdShow);
+
+	// Game-Init
+
+	// Enter the main GAMELOOP:
+	MSG msg;
+
+	while (TRUE)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+
+			if (msg.message == WM_QUIT)
+				break;
+		}
+
+		// Game-Update 
+		// Game-Render
+	}
+
+	// Release pointer, Clean Up 
+
+
+	return msg.wParam;
+}
+
+
+// this is the main message handler for the program
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int wmId, wmEvent;
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	switch (message)
+	{
+	case WM_KEYDOWN:
+		ProcessKey("KeyDown", wParam);
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
 	return 0;
+}
+
+void ProcessKey(const char* type, WPARAM wParam)
+{
+	if (type == "KeyDown")
+	{
+		switch (wParam)
+		{
+		case VK_UP:
+
+			break;
+		case VK_DOWN:
+
+			break;
+		case VK_LEFT:
+
+			break;
+		case VK_RIGHT:
+
+			break;
+		default:
+			break;
+		}
+	}
 }
