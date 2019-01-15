f6a135f1e10e93d32ed303e778fc78f8f02bfe7e
Trần Trung Hiếu <hieu.trantrung1204@icloud.com>
2019-01-15 11:37:44 +0700

    + hello-game message box Windows API

--- source/win32/main.cpp
+++ source/win32/main.cpp
@@ -1,7 +1,17 @@
-#include <iostream>
+#include <windows.h>
 
-void main()
+// the entry point for any Windows program
+int WINAPI WinMain(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow)
 {
-	std::cout << "hello game" << std::endl;
-	system("pause");
+	// create a "Hello World" message box using MessageBox()
+	MessageBox(NULL,
+		"Hello-Game",
+		"Just another Hello World program!",
+		MB_ICONEXCLAMATION | MB_OK);
+
+	// return 0 to Windows
+	return 0;
 }
